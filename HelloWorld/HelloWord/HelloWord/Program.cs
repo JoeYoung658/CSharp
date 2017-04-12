c948c0babe2be64d3ebb9f281dfe62c0b495532c
joeyoung658 <josephyoung658@gmail.com>
2017-04-12 22:36:53 +0100

    Hello World

---
+++ HelloWorld/HelloWord/HelloWord/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+using System.Net.NetworkInformation;
+
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Title = ("This my first C# Applcation");
+            Console.WriteLine("hello world!");
+
+
+            //This starts threads (Can be run at the same time as other code)
+            Thread firstThread = new Thread(new ThreadStart(FirstThread));
+            Thread secondThread = new Thread(new ThreadStart(SecondThread));
+
+            firstThread.Start();
+            secondThread.Start();
+        }
+
+        
+        /// <summary>
+        /// This is the FirstThread Function which contains all the code for the firstThread
+        /// </summary>
+        public static void FirstThread()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("This is the first thread");
+                Thread.Sleep(1000);
+            }
+
+        }
+
+
+        /// <summary>
+        /// Exmaple of another thread running along side main thread
+        /// </summary>
+        public static void SecondThread()
+        {
+            int counter = 0;
+
+            while (counter != 11)
+            {
+                Console.WriteLine("This is the second thread");
+                Thread.Sleep(1000);
+                counter++;
+            }
+        }
+    }
+}
