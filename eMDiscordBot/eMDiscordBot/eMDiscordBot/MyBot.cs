b096090231d4eae28e8600abec960e1c2bf1031e
joeyoung658 <josephyoung658@gmail.com>
2017-05-21 12:26:49 +0100

    eMDiscordBot

---
+++ eMDiscordBot/eMDiscordBot/eMDiscordBot/MyBot.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+
+namespace eMDiscordBot
+{
+    class MyBot
+    {
+
+        DiscordClient discord;
+
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+                {
+                    x.LogLevel = LogSeverity.Info;
+                    x.LogHandler = log;
+
+                });
+
+
+            discord.UsingCommands(x =>
+            {
+                x.AllowMentionPrefix = true;
+
+
+            });
+
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("Hello")
+                .Do(async (e) =>
+                {
+
+                    await e.Channel.SendMessage("Hello, yes this is all that I do.");
+                });
+
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MzE1ODAxMjA1MDg5NjMyMjU4.DAMBLQ.K3QR5CwMy2_QQp6mObLb5qoLPOM", TokenType.Bot);
+            });
+
+            }
+
+        private void log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        }
+    }
+
